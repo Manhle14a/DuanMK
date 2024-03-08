b9b44d6bff01da17f0ee250914b9e526516e137a
Manhle14a <manhlvph40739@fpt.edu.vn>
2024-03-08 17:49:06 +0700

    manh

--- src/Controllers/Client/HomController.php
+++ src/Controllers/Client/HomController.php
@@ -1,18 +1,8 @@
 <?php
-namespace Admin\DuanKm\Controller\Client;
+namespace Admin\DuanKm\Controllers\Client;
 use Admin\DuanKm\Commons\Controller;
 class HomController extends Controller{
-
-    public function index()
-    {
-
-        // $name = "Nguyen Van A";
-        // $email = 'a@gmail.com';
-        // $password = '1234567';
-
-        // $user = new User();
-        // $user->deleteByID(4);
-        // $user->insert($name, $email, $password);
+    public function index(){
         return $this->renderViewClient('home');
     }
 }
